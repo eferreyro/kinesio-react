c6c0107b5eb76d057c828acf4e3bd526980371b7
emmafe7472 <emafe7472@gmail.com>
2019-04-26 21:37:34 -0500

    iniciando repo React

--- src/App.js
+++ src/App.js
@@ -5,20 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+      Mi primer app con React
+      </nav>
+      <img src={logo} className="App-logo" alt="logo" />
+    
     </div>
   );
 }
